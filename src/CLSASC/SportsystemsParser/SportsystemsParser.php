5b63da5eef6fd4b02ca0b9cb5290b88d7a8f33b4
Chris Heppell <clheppell@icloud.com>
2020-01-09 20:32:33 +0000

    Initial commit

---
+++ src/CLSASC/SportsystemsParser/SportsystemsParser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CLSASC\SportsystemsParser;
+
+/**
+ * A PHP Class to parse various Sportsystems files.
+ * 
+ * Some functionality may not be implemented.
+ *
+ * @copyright Chester-le-Street ASC https://github.com/Chester-le-Street-ASC
+ * @author Chris Heppell https://github.com/clheppell
+ */
+class SportsystemsParser {
+
+  /**
+   * Create parser
+   */
+  public function __construct() {
+
+  }
+}
